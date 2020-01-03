dfa4749e2fbe8981760d776429f39a7e2113958c
whisper0077 <tubosaki.shun@gmail.com>
2020-01-03 19:01:52 +0900

    atcoder: DP C,D

--- atcoder/etc/dp/C.py
+++ atcoder/etc/dp/C.py
@@ -1,13 +1,18 @@
-import sys
-sys.setrecursionlimit(10**8)
 
 N = int(input())
 abc = [list(map(int, input().split())) for i in range(N)]
 abc.append([0, 0, 0])
 
 dp = [[-1] * 4 for i in range(N + 1)]
+dp[0] = [0, 0, 0]
+for i in range(1, N + 1):
+    dp[i][0] = max([dp[i - 1][1] + abc[i - 1][1], dp[i - 1][2] + abc[i - 1][2]])
+    dp[i][1] = max([dp[i - 1][0] + abc[i - 1][0], dp[i - 1][2] + abc[i - 1][2]])
+    dp[i][2] = max([dp[i - 1][1] + abc[i - 1][1], dp[i - 1][0] + abc[i - 1][0]])
 
+print(max(dp[N]))
 
+'''全探索
 def happy(n, p):
     if n < 0:
         return 0
@@ -20,6 +25,4 @@ def happy(n, p):
     dp[n][p] = max([a, b, c])
 
     return dp[n][p]
-
-
-print(happy(N - 1, 3))
+'''
