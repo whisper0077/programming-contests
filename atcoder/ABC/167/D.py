e0451a2ab99a2d80d896a51e5bf16e2cff22ed7b
whisper0077 <tubosaki.shun@gmail.com>
2020-05-11 01:01:41 +0900

    atcoder: ABC 167 D ダブリングによる解法追加

--- atcoder/ABC/167/D.py
+++ atcoder/ABC/167/D.py
@@ -1,5 +1,23 @@
 N, K = map(int, input().split())
 *A, = map(int, input().split())
+A = [a-1 for a in A]
+
+# Douling(ダブリング)
+i = 1
+to = [[a for a in A]]
+while pow(2, i) < K:
+    t = [to[i-1][to[i-1][j]] for j in range(N)]
+    to.append(t)
+    i += 1
+
+ans = 0
+for j in range(i):
+    if K & (1 << j) > 0:
+        ans = to[j][ans]
+print(ans+1)
+
+'''
+周期を意識した解法
 s = 0
 l = 0
 n = 0
@@ -22,3 +40,4 @@ while K > 0:
     s = A[s]-1
     K = K-1
 print(s+1)
+'''
