be3b43db30611a45b29a08e971b5709b0c8146e4
whisper0077 <tubosaki.shun@gmail.com>
2020-06-28 00:00:11 +0900

    atcoder: ABC 172 よりよい

--- atcoder/ABC/172/C.py
+++ atcoder/ABC/172/C.py
@@ -2,6 +2,26 @@ import bisect
 N, M, K = map(int, input().split())
 *A, = map(int, input().split())
 *B, = map(int, input().split())
+
+for i in range(1, N):
+    A[i] += A[i-1]
+for i in range(1, M):
+    B[i] += B[i-1]
+
+A = [0]+A
+B = [0]+B
+j = M
+ans = 0
+for i in range(N+1):
+    k = K-A[i]
+    if k < 0:
+        break
+    while B[j] > k:
+        j -= 1
+    ans = max(ans, i+j)
+
+print(ans)
+'''
 for i in range(1, N):
     A[i] += A[i-1]
 for i in range(1, M):
@@ -18,3 +38,4 @@ for i in range(N+1):
         ans = i+j
 
 print(ans)
+'''
--- atcoder/ABC/172/D.py
+++ atcoder/ABC/172/D.py
@@ -1,4 +1,14 @@
 N = int(input())
+# iが何回出現するか考える
+# 1 -> 1,2,3,4
+# 2 -> 2,4
+# 3 -> 3
+ans = 0
+for i in range(1, N+1):
+    ans += i*(N//i)*(N//i+1)//2
+print(ans)
+
+c = 0
 dp = [1]*(N+1)
 ans = 1
 for i in range(2, N+1):
@@ -6,5 +16,6 @@ for i in range(2, N+1):
     while j < N+1:
         dp[j] += 1
         j += i
+        c += 1
     ans += i*dp[i]
-print(ans)
+print(ans, c)
