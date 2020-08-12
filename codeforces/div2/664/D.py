4669124f40390f89a9c3f07cef04875be2241631
whisper0077 <tubosaki.shun@gmail.com>
2020-08-13 04:26:47 +0900

    codeforces: div2 664 D

--- codeforces/div2/664/D.py
+++ codeforces/div2/664/D.py
@@ -1,20 +1,23 @@
+from itertools import accumulate
+
 n, d, m = map(int, input().split())
 *a, = map(int, input().split())
 l, r = [], []
 a.sort()
+a.reverse()
 for i in range(n):
     if a[i] <= m:
         l.append(a[i])
     else:
         r.append(a[i])
-r.reverse()
-r0 = r[0]
-r = r[1:]
+
+rs = [0]+list(accumulate(r))
+ls = [0]+list(accumulate(l))
 
 ans = 0
-for i in range(len(r)+1):
-    t = sum(r[:i])
-    t += sum(l[i*d:])
-    if ans < t:
-        ans = t
-print(ans+r0)
+for i in range(len(ls)):
+    j = (n-i+d)//(d+1)
+    if j > len(r):
+        continue
+    ans = max(ans, ls[i]+rs[j])
+print(ans)
