c967265af9991ac75e77145b0efdae367cfb6b33
whisper0077 <tubosaki.shun@gmail.com>
2020-08-22 02:14:58 +0900

    codeforces: div2 665 C

--- codeforces/div2/665/C.py
+++ codeforces/div2/665/C.py
@@ -2,26 +2,12 @@ for t in range(int(input())):
     n = int(input())
     *a, = map(int, input().split())
     d = min(a)
-
-    b = []
-    for i in range(n):
-        if a[i] % d == 0:
-            b.append(a[i])
-    b.sort()
+    b = sorted(a)
 
     ans = "YES"
-    bi = 0
-    m = 0
     for i in range(n):
-        if a[i] % d == 0:
-            if b[bi] < m:
+        if a[i] != b[i]:
+            if b[i] % d != 0:
                 ans = "NO"
                 break
-            m = b[bi]
-            bi += 1
-        else:
-            if a[i] < m:
-                ans = "NO"
-                break
-            m = a[i]
     print(ans)
