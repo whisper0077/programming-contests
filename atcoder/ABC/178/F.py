cd98e2dae416d6620cec593258d6ce6739e2ea17
whisper0077 <tubosaki.shun@gmail.com>
2020-09-14 01:03:38 +0900

    atcoder: ABC178 F2

--- atcoder/ABC/178/F.py
+++ atcoder/ABC/178/F.py
@@ -1,6 +1,39 @@
 N = int(input())
 *A, = map(int, input().split())
 *B, = map(int, input().split())
+B.reverse()
+
+ac = [0]*(N+1)
+bc = [0]*(N+1)
+
+ok = True
+for i in range(N):
+    ac[A[i]] += 1
+    bc[B[i]] += 1
+
+for i in range(1, N+1):
+    if (ac[i]+bc[i]) > N:
+        ok = False
+        break
+
+if ok:
+    l, r = 0, N-1
+    for i in range(N):
+        if A[i] == B[i]:
+            if A[i] != B[l] and A[l] != B[i]:
+                B[l], B[i] = B[i], B[l]
+                l += 1
+            elif A[i] != B[r] and A[r] != B[i]:
+                B[r], B[i] = B[i], B[r]
+                r -= 1
+    print("Yes")
+    print(*B)
+else:
+    print("No")
+'''
+N = int(input())
+*A, = map(int, input().split())
+*B, = map(int, input().split())
 
 ac = [0]*(N+1)
 bc = [0]*(N+1)
@@ -24,3 +57,4 @@ if ok:
     print(*[B[(i-d) % N] for i in range(N)])
 else:
     print("No")
+'''
