f5edd7e0d5ad7c56938a7ea6cc9bcdf2eb54260d
whisper0077 <tubosaki.shun@gmail.com>
2020-12-14 00:30:06 +0900

    配るDP

--- atcoder/ABC/185/E.py
+++ atcoder/ABC/185/E.py
@@ -2,6 +2,19 @@ N, M = map(int, input().split())
 *A, = map(int, input().split())
 *B, = map(int, input().split())
 
+dp = [[10**9 for j in range(M+1)] for i in range(N+1)]
+dp[0][0] = 0
+
+for i in range(N+1):
+    for j in range(M+1):
+        if i < N:
+            dp[i+1][j] = min(dp[i+1][j], dp[i][j]+1)
+        if j < M:
+            dp[i][j+1] = min(dp[i][j+1], dp[i][j]+1)
+        if i < N and j < M:
+            dp[i+1][j+1] = min(dp[i+1][j+1], dp[i][j]+(A[i] != B[j]))
+'''
+もらうDP
 dp = [[max(i, j) for j in range(M+1)] for i in range(N+1)]
 
 for i in range(1, N+1):
@@ -11,5 +24,6 @@ for i in range(1, N+1):
             dp[i-1][j]+1,
             dp[i][j-1]+1
         )
+'''
 
 print(dp[N][M])
