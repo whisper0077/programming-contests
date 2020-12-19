4a483a2846dcdeb10529094879e1ee85532dd530
whisper0077 <tubosaki.shun@gmail.com>
2020-12-20 01:26:26 +0900

    説明追加

--- atcoder/ABC/186/E.py
+++ atcoder/ABC/186/E.py
@@ -3,26 +3,31 @@ from math import gcd
 '''
 s + k*x ≡ 0 mod n を解く
 a = k
-b = (n-s)
+b = n-s
 とすると
 ax ≡ b mod n
-よって
-mod nにおけるaの逆元を求める問題
+ここでd=gcd(a,n)とすると
+ax/d ≡ b/d mod n/d
+となり、b%d!=0のとき解を持たない
+
+後はmod nにおけるaの逆元を求める問題
 '''
 
 for t in range(int(input())):
     n, s, k = map(int, input().split())
     a = k
-    b = -s
+    b = n-s
     p = n
 
-    # gcdして簡略化
-    d = gcd(a, gcd(b, p))
-    a //= d
-    b //= d
-    p //= d
-
-    if gcd(a, p) != 1:
+    d = gcd(a, n)
+    if b % d != 0:
         print(-1)
     else:
-        print(pow(a, -1, p)*b % p)
+        a //= d
+        b //= d
+        p //= d
+
+        if gcd(a, p) != 1:
+            print(-1)
+        else:
+            print(pow(a, -1, p)*b % p)
