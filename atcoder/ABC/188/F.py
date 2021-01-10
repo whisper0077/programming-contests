7e96a33b34889f1643395a9fb27135cd54d5da57
whisper0077 <tubosaki.shun@gmail.com>
2021-01-11 00:50:35 +0900

    atcoder: ABC 188 F解説追加

--- atcoder/ABC/188/F.py
+++ atcoder/ABC/188/F.py
@@ -1,6 +1,22 @@
 from functools import lru_cache
 X, Y = map(int, input().split())
 
+'''
+YをXにする操作と考える
+A -> +1
+B -> -1
+C -> /2
+とすると、
+AB,BA -> 何もしない
+AAC,BBC -> CA,CBに置き換え可能
+上記操作は考えなくて良くなる
+
+よって遷移はC or AC or BCで、2で割り切れるかどうかで変わる
+操作回数は |X-y| or C(+1) or AC(+2) or BC(+2)となる
+
+毎回関数を呼び出したときの引数の変化先はたかだか2個で、//2されていくのでlog(Y)個程度しかない
+'''
+
 
 @lru_cache(None)
 def solve(y):
