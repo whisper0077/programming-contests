54b81b21d827fa604e38f3b7578aa7039f3d132c
whisper0077 <tubosaki.shun@gmail.com>
2021-02-28 00:39:42 +0900

    コメント追加

--- atcoder/ABC/193/E.py
+++ atcoder/ABC/193/E.py
@@ -45,6 +45,10 @@ def crt(r, m):
 
 INF = 10**20
 for t in range(int(input())):
+    '''
+    t mod (2X+2Y) = t1 -> t ≡ t1 (mod (2X+2Y))
+    t mod (P+Q) = t2   -> t ≡ t2 (mod (P+Q))
+    '''
     x, y, p, q = map(int, input().split())
     ans = INF
     for t1 in range(x, x+y):
