b078f563845d52fe6879a53b3b042db6428bde51
whisper0077 <tubosaki.shun@gmail.com>
2021-03-06 23:48:17 +0900

    atcoder: ABC 194 E シンプル

--- atcoder/ABC/194/E.py
+++ atcoder/ABC/194/E.py
@@ -1,28 +1,14 @@
-import heapq
-
+from collections import Counter
 N, M = map(int, input().split())
 *A, = map(int, input().split())
-m = max(A)+1
-v = [0]*(m+1)
-for i in range(M-1):
-    v[A[i]] += 1
+c = Counter(A[:M])
+ans = 0
+while c[ans] != 0:
+    ans += 1
 
-q = []
-for i in range(m+1):
-    if v[i] == 0:
-        heapq.heappush(q, i)
-
-ans = m
-for i in range(N-M+1):
-    v[A[i+M-1]] += 1
-    while q:
-        if v[q[0]] == 0:
-            ans = min(ans, q[0])
-            break
-        else:
-            heapq.heappop(q)
-    v[A[i]] -= 1
-    if v[A[i]] <= 0:
-        heapq.heappush(q, A[i])
-
+for i in range(N-M):
+    c[A[i+M]] += 1
+    c[A[i]] -= 1
+    if c[A[i]] == 0:
+        ans = min(ans, A[i])
 print(ans)
